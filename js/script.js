29905b225580d85cc5aba41fba2721ef7b5808bf
Malgorzata Lesniak <M999@interia.eu>
2019-04-09 23:43:12 +0200

    All buttons active

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-function Phone(brand, price, color, image) {
+function Phone(id, brand, price, color, image) {
+    this.id = id;
 	this.brand = brand;
 	//by using this, the "brand" property of the object we create will assume the value of the brand argument
 	this.price = price;
@@ -17,16 +18,13 @@ Phone.prototype = {
         Mustache.parse(template);
         element.innerHTML = Mustache.render(template, this);
         document.getElementById('phones').innerHTML += element.innerHTML;
-        document.querySelector('button').addEventListener('click', function (event) {
-            document.getElementById('info').innerHTML = '<ul> kolor:'+self.color+' <br> cena: '+self.price+'</ul>';
-        });
     }
 };
 
   
-var SamsungGalaxyS6 = new Phone('Samsung', 189, 'gold', 'https://images.samsung.com/is/image/samsung/nl-galaxy-s6-g920f-sm-g920fzdaphn-001-front-gold?$PD_GALLERY_L_JPG$');
-var iPhone6S = new Phone('Apple', 314,'black', 'https://bsimg.nl/images/2/base_Apple-iPhone-6S-32GB-Black_1.jpg');
-var OnePlusOne = new Phone('OnePlus', 250, 'white', 'https://bsimg.nl/images/2/base_OnePlus-One-White_1.jpg');
+var SamsungGalaxyS6 = new Phone(1,'Samsung', 189, 'gold', 'https://images.samsung.com/is/image/samsung/nl-galaxy-s6-g920f-sm-g920fzdaphn-001-front-gold?$PD_GALLERY_L_JPG$');
+var iPhone6S = new Phone(2,'Apple', 314,'black', 'https://bsimg.nl/images/2/base_Apple-iPhone-6S-32GB-Black_1.jpg');
+var OnePlusOne = new Phone(3,'OnePlus', 250, 'white', 'https://bsimg.nl/images/2/base_OnePlus-One-White_1.jpg');
 
 SamsungGalaxyS6.printInfo();
 SamsungGalaxyS6.showPhone();
@@ -36,3 +34,30 @@ iPhone6S.showPhone();
 
 OnePlusOne.printInfo();
 OnePlusOne.showPhone();
+
+var buttons = document.querySelectorAll('button');
+var infos = document.querySelectorAll('p');
+var kolor, cena;
+for ( var i=0; i<buttons.length; i++) {
+    buttons[i].id = i;
+    infos[i].id = i;
+    buttons[i].addEventListener('click', function (event) {
+        if (event.target.id == 0) {
+            kolor = SamsungGalaxyS6.color;
+            cena = SamsungGalaxyS6.price;
+        };
+        if (event.target.id == 1) {
+            kolor = iPhone6S.color;
+            cena = iPhone6S.price;
+        };
+        if (event.target.id == 2) {
+            kolor = OnePlusOne.color;
+            cena = OnePlusOne.price;
+        };
+        for (var j=0; j<infos.length; j++) {
+            infos[j].innerHTML = '<br><br>';
+        };
+        infos[event.target.id].innerHTML = '<ul> kolor:'+kolor+' <br> cena: '+cena+'</ul>';
+});
+};
+
